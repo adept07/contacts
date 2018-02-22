3c714f2d99193124761d7c93361db0d60c73c443
adept07 <satyavarma@gmail.com>
2018-02-22 08:19:53 -0800

    remoted commented code

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(request, response, next) {
-    //response.send('Index page');
   response.render('index.html');
 });
 
